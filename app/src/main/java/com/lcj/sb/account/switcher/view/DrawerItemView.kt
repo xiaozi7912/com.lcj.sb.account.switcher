31997f8459b667c22ca35caa141d7593c650a1a1
larryliu <larryliu@insynerger.com>
2020-01-17 12:18:59 +0800

    -新增 檢查app版本 -新增 日版apk下載

--- app/src/main/java/com/lcj/sb/account/switcher/view/DrawerItemView.kt
+++ app/src/main/java/com/lcj/sb/account/switcher/view/DrawerItemView.kt
@@ -3,6 +3,7 @@ package com.lcj.sb.account.switcher.view
 import android.content.Context
 import android.util.AttributeSet
 import android.view.View
+import android.widget.ImageButton
 import android.widget.ImageView
 import android.widget.LinearLayout
 import android.widget.TextView
@@ -11,6 +12,7 @@ import com.lcj.sb.account.switcher.R
 class DrawerItemView : LinearLayout {
     private lateinit var mIconView: ImageView
     private lateinit var mTitleView: TextView
+    private lateinit var mApkDownloadButton: ImageButton
 
     private var mIconId: Int = 0
     private lateinit var mTitleStr: String
@@ -27,6 +29,7 @@ class DrawerItemView : LinearLayout {
         View.inflate(context, R.layout.drawer_item_view, this)
         mIconView = findViewById(R.id.drawer_item_icon)
         mTitleView = findViewById(R.id.drawer_item_title)
+        mApkDownloadButton = findViewById(R.id.drawer_item_apk_download_btn)
 
         if (attrs != null) {
             context!!.theme.obtainStyledAttributes(attrs, R.styleable.DrawerItemView, 0, 0)
@@ -49,5 +52,17 @@ class DrawerItemView : LinearLayout {
 
     fun getTitle(): String {
         return mTitleStr
+    }
+
+    fun setDownloadAPKButtonVisibility(isVisible: Boolean) {
+        if (isVisible) {
+            mApkDownloadButton.visibility = View.VISIBLE
+        } else {
+            mApkDownloadButton.visibility = View.INVISIBLE
+        }
+    }
+
+    fun setDownloadAPKButtonClickListener(listener: OnClickListener) {
+        mApkDownloadButton.setOnClickListener(listener)
     }
 }
