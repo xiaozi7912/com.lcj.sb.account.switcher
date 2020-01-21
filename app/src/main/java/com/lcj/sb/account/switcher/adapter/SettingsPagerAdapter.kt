c8a7a9da98445a05756510a7e6b0d3055ed85677
larryliu <larryliu@insynerger.com>
2020-01-21 17:50:28 +0800

    -修改 顯示文字

--- app/src/main/java/com/lcj/sb/account/switcher/adapter/SettingsPagerAdapter.kt
+++ app/src/main/java/com/lcj/sb/account/switcher/adapter/SettingsPagerAdapter.kt
@@ -2,12 +2,13 @@ package com.lcj.sb.account.switcher.adapter
 
 import androidx.fragment.app.Fragment
 import androidx.viewpager2.adapter.FragmentStateAdapter
+import com.lcj.sb.account.switcher.BuildConfig
 import com.lcj.sb.account.switcher.fragment.LocalBackupFragment
 import com.lcj.sb.account.switcher.fragment.RemoteBackupFragment
 
 class SettingsPagerAdapter(fragment: Fragment) : FragmentStateAdapter(fragment) {
     override fun getItemCount(): Int {
-        return 2
+        return if (BuildConfig.DEBUG) 2 else 1
     }
 
     override fun createFragment(position: Int): Fragment {
