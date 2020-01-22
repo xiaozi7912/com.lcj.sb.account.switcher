aadae934578434e251731b0388e87ae95da4fa9f
larryliu <larryliu@insynerger.com>
2020-01-22 17:10:41 +0800

    -修改 取消判斷是否為debug模式

--- app/src/main/java/com/lcj/sb/account/switcher/adapter/SettingsPagerAdapter.kt
+++ app/src/main/java/com/lcj/sb/account/switcher/adapter/SettingsPagerAdapter.kt
@@ -2,13 +2,12 @@ package com.lcj.sb.account.switcher.adapter
 
 import androidx.fragment.app.Fragment
 import androidx.viewpager2.adapter.FragmentStateAdapter
-import com.lcj.sb.account.switcher.BuildConfig
 import com.lcj.sb.account.switcher.fragment.LocalSyncFragment
 import com.lcj.sb.account.switcher.fragment.RemoteSyncFragment
 
 class SettingsPagerAdapter(fragment: Fragment) : FragmentStateAdapter(fragment) {
     override fun getItemCount(): Int {
-        return if (BuildConfig.DEBUG) 2 else 1
+        return 2
     }
 
     override fun createFragment(position: Int): Fragment {
