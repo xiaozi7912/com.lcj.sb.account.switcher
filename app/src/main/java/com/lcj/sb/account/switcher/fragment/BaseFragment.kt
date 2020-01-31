16f1c0d451633fad0796a07dcc0ddc5fff781fe3
larryliu7912 <larryliu7912@gmail.com>
2020-01-31 23:22:57 +0800

    -修正 取得遊戲packagename錯誤

--- app/src/main/java/com/lcj/sb/account/switcher/fragment/BaseFragment.kt
+++ app/src/main/java/com/lcj/sb/account/switcher/fragment/BaseFragment.kt
@@ -7,7 +7,6 @@ import android.preference.PreferenceManager
 import android.util.Log
 import android.view.View
 import androidx.fragment.app.Fragment
-import com.google.firebase.auth.FirebaseUser
 import com.lcj.sb.account.switcher.database.entity.Account
 import com.lcj.sb.account.switcher.utils.Configs
 
@@ -21,23 +20,11 @@ open class BaseFragment : Fragment() {
 
     lateinit var mContentView: View
 
-    protected lateinit var mCurrentLang: Account.Language
-    protected lateinit var mCurrentUser: FirebaseUser
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-//        mCurrentUser = FirebaseAuth.getInstance().currentUser!!
-    }
-
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
         Log.i(LOG_TAG, "onActivityCreated")
         mActivity = activity!!
         mContentView = mActivity.findViewById(android.R.id.content)
-
-        PreferenceManager.getDefaultSharedPreferences(mActivity).apply {
-            mCurrentLang = Account.Language.valueOf(getString(Configs.PREF_KEY_LANGUAGE, Account.Language.JP.name)!!)
-        }
     }
 
     override fun onStart() {
@@ -50,9 +37,13 @@ open class BaseFragment : Fragment() {
         Log.i(LOG_TAG, "onResume")
     }
 
-    fun getPackageName(): String = when (mCurrentLang) {
-        Account.Language.JP -> Configs.PREFIX_NAME_SB_JP
-        Account.Language.TW -> Configs.PREFIX_NAME_SB_TW
+    fun getPackageName(): String {
+        PreferenceManager.getDefaultSharedPreferences(mActivity).let {
+            return when (Account.Language.valueOf(it.getString(Configs.PREF_KEY_LANGUAGE, Account.Language.JP.name)!!)) {
+                Account.Language.JP -> Configs.PREFIX_NAME_SB_JP
+                Account.Language.TW -> Configs.PREFIX_NAME_SB_TW
+            }
+        }
     }
 
     fun startApplication(appId: String) {
