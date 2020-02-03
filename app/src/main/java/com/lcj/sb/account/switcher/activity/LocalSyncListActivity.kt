98967325fb57d458691700e8d6aa34a4e8b50f51
larryliu <larryliu@insynerger.com>
2020-02-03 18:01:06 +0800

    -加入手勢

--- app/src/main/java/com/lcj/sb/account/switcher/activity/LocalSyncListActivity.kt
+++ app/src/main/java/com/lcj/sb/account/switcher/activity/LocalSyncListActivity.kt
@@ -2,6 +2,9 @@ package com.lcj.sb.account.switcher.activity
 
 import android.os.Bundle
 import android.util.Log
+import android.view.GestureDetector
+import android.view.MotionEvent
+import androidx.core.view.GestureDetectorCompat
 import androidx.databinding.DataBindingUtil
 import com.google.android.gms.ads.AdRequest
 import com.lcj.sb.account.switcher.BaseActivity
@@ -13,17 +16,39 @@ import com.lcj.sb.account.switcher.utils.Configs
 class LocalSyncListActivity : BaseActivity() {
     private lateinit var mBinding: ActivityLocalSyncListBinding
 
+    private lateinit var mDetector: GestureDetectorCompat
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         mBinding = DataBindingUtil.setContentView(mActivity, R.layout.activity_local_sync_list)
 
         setSupportActionBar(mBinding.toolBar)
+        mDetector = GestureDetectorCompat(this, object : GestureDetector.SimpleOnGestureListener() {
+            override fun onFling(e1: MotionEvent?, e2: MotionEvent?, velocityX: Float, velocityY: Float): Boolean {
+                Log.d(LOG_TAG, "onFling: $e1 $e2")
+                Log.d(LOG_TAG, "onFling: $velocityX $velocityY")
+                return super.onFling(e1, e2, velocityX, velocityY)
+            }
+        })
+
         initView()
         reloadAd()
     }
 
+    override fun onSupportNavigateUp(): Boolean {
+        onBackPressed()
+        return super.onSupportNavigateUp()
+    }
+
+    override fun onTouchEvent(event: MotionEvent?): Boolean {
+        mDetector.onTouchEvent(event)
+        return super.onTouchEvent(event)
+    }
+
     override fun initView() {
         Log.v(LOG_TAG, "Language : ${intent.getIntExtra(Configs.INTENT_KEY_LANGUAGE, Account.Language.JP.ordinal)}")
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        supportActionBar?.setDisplayShowHomeEnabled(true)
     }
 
     override fun reloadAd() {
